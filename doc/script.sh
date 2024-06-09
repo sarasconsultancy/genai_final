201442236df5443603f3abc2e12cc42395b3ddc0
xerxez solutions <info@xerxez.in>
2024-06-09 12:43:42 +0530

    Changes in Terraform

--- doc/script.sh
+++ doc/script.sh
@@ -8,7 +8,6 @@ sudo systemctl enable docker
 sudo usermod -aG docker ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
-
 #Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
